c02ece5d8e062957a3fff82ce431a3d133e22a42
nguyenbahung94 <mr.hungcity@gmail.com>
2024-08-19 22:54:03 +0700

    aplly plugin feature

--- list/build.gradle.kts
+++ list/build.gradle.kts
@@ -1,15 +1,11 @@
 plugins {
-    alias(libs.plugins.composeln.library)
-    alias(libs.plugins.composeln.kotlin.android)
-    alias(libs.plugins.composeln.hilt)
+    alias(libs.plugins.composeln.feature)
 }
 
 android {
     namespace = libs.plugins.listNameSpace.get().toString()
 }
 dependencies {
-    implementation(projects.core)
-    implementation(projects.network)
 
     implementation(libs.retrofit.core)
 
